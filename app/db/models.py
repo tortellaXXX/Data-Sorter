b84eee539e2f243d518d2a78b5dfc08b83239901
tortellaXXX <loloxlolox36@gmail.com>
2025-09-30 14:56:03 +0300

    Deleted Dremio -> Postgres

--- app/db/models.py
+++ app/db/models.py
@@ -1,12 +1,13 @@
 from sqlalchemy import Column, String
 from .session import Base
 
+
 class UserTable(Base):
     """
-    Таблица для мультипользовательского режима
-    session_id -> имя таблицы в Dremio
+    Таблица для мультипользовательского режима:
+    session_id -> имя временной таблицы в Postgres
     """
     __tablename__ = "user_tables"
 
     session_id = Column(String, primary_key=True, index=True)
-    dremio_table = Column(String, nullable=False)
+    table_name = Column(String, nullable=False)
